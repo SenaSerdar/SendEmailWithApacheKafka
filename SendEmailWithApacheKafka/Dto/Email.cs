2957b4ade52db2ac8ea45d1183de8522fb8302bd
senaserdar <sena@pembepanjur.com>
2023-01-17 12:57:37 +0300

    created producer and consumer

---
+++ SendEmailWithApacheKafka/Dto/Email.cs
@@ -0,0 +1,7 @@
+namespace SendEmailWithApacheKafka.Dto;
+
+public class Email
+{
+    public string To { get; set; }
+    public string Subject { get; set; }
+}
--- SendEmailWithApacheKafka/Producer.cs
+++ SendEmailWithApacheKafka/Producer.cs
@@ -1,6 +1,44 @@
+using Confluent.Kafka;
+using Newtonsoft.Json;
+using SendEmailWithApacheKafka.Dto;
+
 namespace SendEmailWithApacheKafka;
 
 public class Producer
 {
-    
+    static async Task Main(string[] args)
+    {
+       await EmailProducerAsync();
+    }
+    public static async Task EmailProducerAsync()
+    {
+
+        var config = new ProducerConfig
+        {
+            BootstrapServers = "localhost:9092"
+        };
+        using var producer = new ProducerBuilder<Null,string>(config).Build();
+        try
+        {
+            while (true)
+            {
+                Console.Write("To:");
+                var to = Console.ReadLine();
+                Console.Write("\nSubject:");
+                var subject = Console.ReadLine();
+                var email = new Email
+                {
+                    To=to,
+                    Subject = subject
+                };
+                var emailMessage = JsonConvert.SerializeObject(email);
+                var result = await producer.ProduceAsync("testTopic",new Message<Null, string>{Value = emailMessage});
+                Console.WriteLine($"Sent:{result.Value} Topic: {result.TopicPartitionOffset} \n");
+            }
+        }
+        catch (ProduceException<Null,string> e)
+        {
+            Console.WriteLine(e.Error.Reason);
+        }
+    }
 }
